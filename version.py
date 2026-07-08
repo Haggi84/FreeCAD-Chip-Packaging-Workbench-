a5294912bdca228dc29ca12d5b346de9e93f61f9
Zeitler Jochen <jochen.zeitler@faps.fau.de>
2026-07-08 12:26:36 +0200

    new versioning

--- version.py
+++ version.py
@@ -10,8 +10,8 @@ Follows Semantic Versioning (https://semver.org/):
 """
 
 MAJOR = 0
-MINOR = 6
-PATCH = 2
+MINOR = 7
+PATCH = 0
 
 VERSION = (MAJOR, MINOR, PATCH)
 VERSION_STRING = f"{MAJOR}.{MINOR}.{PATCH}"  
